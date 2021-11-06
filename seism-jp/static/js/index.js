7e5c301e8854aa623cc8034132add88cf05da812
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2021-11-06 10:32:41 +0900

    fixed zoom level

--- seism-jp/static/js/index.js
+++ seism-jp/static/js/index.js
@@ -17,13 +17,13 @@ const features = JSON.parse(geoData.value)['features'];
 
 // init.
 const center = [36.2048, 138.2529];
-const zoom = 5;
+const zoom = 8;
 const map = L.map('map').setView(center, zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-  maxZoom: 10,
-  minZoom: 5,
+  maxZoom: 12,
+  minZoom: zoom,
 }).addTo(map);
 
 // marker.

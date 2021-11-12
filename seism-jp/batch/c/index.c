f17d57168222d63eef7bf6f4e7dc04849a9a2eaf
hirotoyoshidome <yoshidome.hiroto@bizpa.co.jp>
2021-11-12 09:12:31 +0900

    fixed

--- seism-jp/batch/c/index.c
+++ seism-jp/batch/c/index.c
@@ -1,9 +1,38 @@
 #include<stdio.h>
+#include <stdlib.h>
 
+#define N 256
 
 // gcc -o index index.c
 
 int main(void) {
-  printf("HelloWorld!\n"); 
+  printf("HelloWorld!\n");
+
+  FILE *fp;
+  char filename[] = "./sample.txt";
+//   int chr;
+  char str[N];
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    printf("%s is opening error.\n", filename);
+	return -1;
+  } else {
+    printf("%s is opened\n", filename);
+  }
+
+//   while((chr = fgetc(fp)) != EOF) {
+//     putchar(chr);
+//   }
+
+  while(fgets(str, N, fp) != NULL) {
+      printf("%s", str);
+  }
+
+  printf("%d\n", N);
+
+
+  fclose(fp);
+
   return 0;
 }

67fdaa0ed81b240a82aaa9f0f14ac00c5bea4dae
hirotoyoshidome <yoshidome.hiroto@bizpa.co.jp>
2021-11-12 10:40:01 +0900

    added cpp link

---
+++ seism-jp/batch/cpp/main2.cpp
@@ -0,0 +1,15 @@
+#include "hello.hpp"
+
+
+// compile. -> create hello.o, main2.o
+//  g++ -c main2.cpp hello.cpp
+
+// link. -> create a.out
+// g++ -o a.out main2.o hello.o
+
+// exec.
+// /a.out
+
+int main() {
+    hello();
+}

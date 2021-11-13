070e3c38703c8fb71d3af42c891c5604757eb380
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2021-11-13 12:17:59 +0900

    fixed fromat

--- simulator/bullet/main.py
+++ simulator/bullet/main.py
@@ -20,23 +20,25 @@ p.changeDynamics(humanoid, -1, linearDamping=0, angularDamping=0)
 
 # humanoid param.
 for j in range(p.getNumJoints(humanoid)):
-  p.changeDynamics(humanoid, j, linearDamping=0, angularDamping=0)
-  info = p.getJointInfo(humanoid, j)
-  jointName = info[1]
-  jointType = info[2]
-  if (jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE):
-    jointIds.append(j)
-    paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4, 0))
+    p.changeDynamics(humanoid, j, linearDamping=0, angularDamping=0)
+    info = p.getJointInfo(humanoid, j)
+    jointName = info[1]
+    jointType = info[2]
+    if jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE:
+        jointIds.append(j)
+        paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4, 0))
 
 # simulator
 p.setRealTimeSimulation(1)
-while (1):
-  p.setGravity(0, 0, p.readUserDebugParameter(gravId))
-  for i in range(len(paramIds)):
-    c = paramIds[i]
-    targetPos = p.readUserDebugParameter(c)
-    p.setJointMotorControl2(humanoid, jointIds[i], p.POSITION_CONTROL, targetPos, force=5 * 240.)
-  time.sleep(0.01)
+while 1:
+    p.setGravity(0, 0, p.readUserDebugParameter(gravId))
+    for i in range(len(paramIds)):
+        c = paramIds[i]
+        targetPos = p.readUserDebugParameter(c)
+        p.setJointMotorControl2(
+            humanoid, jointIds[i], p.POSITION_CONTROL, targetPos, force=5 * 240.0
+        )
+    time.sleep(0.01)
 
 # startPos = [0,0,1]
 # startOrientation = p.getQuaternionFromEuler([0,0,0])

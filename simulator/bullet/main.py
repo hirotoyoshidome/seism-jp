4329c1ca4a13aa27849f6420be9b2c06eb7144aa
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2021-11-13 15:22:41 +0900

    fixed

--- simulator/bullet/main.py
+++ simulator/bullet/main.py
@@ -2,23 +2,21 @@ import pybullet as p
 import time
 import pybullet_data
 
-
-physicsClient = p.connect(p.GUI)
+# init
+p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+gravId = p.addUserDebugParameter("gravity", -10, 10, -10)
+p.setPhysicsEngineParameter(numSolverIterations=10)
+
+# humanoid
 obUids = p.loadMJCF("mjcf/humanoid.xml")
 humanoid = obUids[1]
-
-gravId = p.addUserDebugParameter("gravity", -10, 10, -1)
-jointIds = []
-paramIds = []
-
-# planeId = p.loadURDF("plane.urdf")
-
-p.setPhysicsEngineParameter(numSolverIterations=10)
 p.changeDynamics(humanoid, -1, linearDamping=0, angularDamping=0)
 
 # humanoid param.
+jointIds = []
+paramIds = []
 for j in range(p.getNumJoints(humanoid)):
     p.changeDynamics(humanoid, j, linearDamping=0, angularDamping=0)
     info = p.getJointInfo(humanoid, j)
@@ -28,10 +26,24 @@ for j in range(p.getNumJoints(humanoid)):
         jointIds.append(j)
         paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4, 0))
 
+# cube
+x = 4
+y = 4
+z = 4
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+for i in range(x):
+    for j in range(y):
+        for k in range(z):
+            pos = [i + 1, j + 1, k + 1]
+            p.loadURDF("cube.urdf", pos, startOrientation)
+
+# R2D2 from star wars.
+p.loadURDF("r2d2.urdf", [-1, -1, 1], startOrientation)
+
 # simulator
 p.setRealTimeSimulation(1)
 while 1:
-    p.setGravity(0, 0, p.readUserDebugParameter(gravId))
+    p.setGravity(0, 0, -10)
     for i in range(len(paramIds)):
         c = paramIds[i]
         targetPos = p.readUserDebugParameter(c)
@@ -39,16 +51,5 @@ while 1:
             humanoid, jointIds[i], p.POSITION_CONTROL, targetPos, force=5 * 240.0
         )
     time.sleep(0.01)
-
-# startPos = [0,0,1]
-# startOrientation = p.getQuaternionFromEuler([0,0,0])
-# boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
-
-# #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-# for i in range (10000):
-#     p.stepSimulation()
-#     time.sleep(1./240.)
-# cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-# print("cubePos : ",cubePos,cubeOrn)
 
 p.disconnect()

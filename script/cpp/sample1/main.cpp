a962b5afe5a19bfcad380e6e479aadcb922babe4
hirotoyoshidome <yoshidome.hiroto@bizpa.co.jp>
2021-11-22 15:13:20 +0900

    fixed dir

--- script/cpp/sample1/main.cpp
+++ script/cpp/sample1/main.cpp
@@ -1,11 +1,11 @@
 #include "hello.hpp"
 #include "greet.hpp"
 
-// compile. -> create hello.o, main2.o
-//  g++ -c main2.cpp hello.cpp greet.cpp
+// compile. -> create hello.o, main.o
+// g++ -c main.cpp hello.cpp greet.cpp
 
 // link. -> create a.out
-// g++ -o a.out main2.o hello.o greet.cpp
+// g++ -o a.out main.o hello.o greet.cpp
 
 // exec.
 // /a.out
